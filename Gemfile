02294fee2ffe12e79f96b95c82d714ba955bd63f
Casper Thomsen <ct@clearhaus.com>
2014-02-05 09:02:23 +0100

    More

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'bigdecimal', '>= 1.2.5'
+
 group :test do
   gem 'rspec'
   gem 'rake'
--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,17 +1,25 @@
 require 'bigdecimal'
 
 describe 'One number' do
-  let(:number) { BigDecimal.new(1.82380.to_s) }
+  let(:number) { BigDecimal.new(1.3574.to_s) }
 
-  it '1/number should be correct' do
-    expect((1/number).to_s).to eq('0.548305735277991007785941441E0')
+  it '1/1.3574 should be correct' do
+    expect((1/number).to_s).to eq('0.736702519522616767349344335E0')
   end
 end
 
 describe 'Another number' do
   let(:number) { BigDecimal.new(0.82380.to_s) }
 
-  it '1/number should be correct' do
+  it '1/0.82380 should be correct' do
+    expect((1/number).to_s).to eq('0.1213886866E1')
+  end
+end
+
+describe 'Third number' do
+  let(:number) { BigDecimal.new(0.8238.to_s) }
+
+  it '1/0.8238 should be correct' do
     expect((1/number).to_s).to eq('0.1213886866E1')
   end
 end
